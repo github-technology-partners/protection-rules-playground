3ac0306fc14900fc913c0852cc7dadc3de13c64e
Alexis Abril <alexisabril@github.com>
2023-11-06 21:02:49 -0800

    Removing custom errors

--- .pulumi/aws/index.ts
+++ .pulumi/aws/index.ts
@@ -36,58 +36,54 @@ const bucketFolder = new synced_folder.S3BucketFolder("bucket-folder", {
 }, { dependsOn: [ownershipControls, publicAccessBlock]});
 
 // Create a CloudFront CDN to distribute and cache the website.
-// const cdn = new aws.cloudfront.Distribution("cdn", {
-//     enabled: true,
-//     origins: [{
-//         originId: bucket.arn,
-//         domainName: bucket.websiteEndpoint,
-//         customOriginConfig: {
-//             originProtocolPolicy: "http-only",
-//             httpPort: 80,
-//             httpsPort: 443,
-//             originSslProtocols: ["TLSv1.2"],
-//         },
-//     }],
-//     defaultCacheBehavior: {
-//         targetOriginId: bucket.arn,
-//         viewerProtocolPolicy: "redirect-to-https",
-//         allowedMethods: [
-//             "GET",
-//             "HEAD",
-//             "OPTIONS",
-//         ],
-//         cachedMethods: [
-//             "GET",
-//             "HEAD",
-//             "OPTIONS",
-//         ],
-//         defaultTtl: 600,
-//         maxTtl: 600,
-//         minTtl: 600,
-//         forwardedValues: {
-//             queryString: true,
-//             cookies: {
-//                 forward: "all",
-//             },
-//         },
-//     },
-//     priceClass: "PriceClass_100",
-//     customErrorResponses: [{
-//         errorCode: 404,
-//         responseCode: 404
-//     }],
-//     restrictions: {
-//         geoRestriction: {
-//             restrictionType: "none",
-//         },
-//     },
-//     viewerCertificate: {
-//         cloudfrontDefaultCertificate: true,
-//     },
-// });
+const cdn = new aws.cloudfront.Distribution("cdn", {
+    enabled: true,
+    origins: [{
+        originId: bucket.arn,
+        domainName: bucket.websiteEndpoint,
+        customOriginConfig: {
+            originProtocolPolicy: "http-only",
+            httpPort: 80,
+            httpsPort: 443,
+            originSslProtocols: ["TLSv1.2"],
+        },
+    }],
+    defaultCacheBehavior: {
+        targetOriginId: bucket.arn,
+        viewerProtocolPolicy: "redirect-to-https",
+        allowedMethods: [
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        ],
+        cachedMethods: [
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        ],
+        defaultTtl: 600,
+        maxTtl: 600,
+        minTtl: 600,
+        forwardedValues: {
+            queryString: true,
+            cookies: {
+                forward: "all",
+            },
+        },
+    },
+    priceClass: "PriceClass_100",
+    restrictions: {
+        geoRestriction: {
+            restrictionType: "none",
+        },
+    },
+    viewerCertificate: {
+        cloudfrontDefaultCertificate: true,
+    },
+});
 
 // Export the URLs and hostnames of the bucket and distribution.
 export const originURL = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
 export const originHostname = bucket.websiteEndpoint;
-// export const cdnURL = pulumi.interpolate`https://${cdn.domainName}`;
-// export const cdnHostname = cdn.domainName;
+export const cdnURL = pulumi.interpolate`https://${cdn.domainName}`;
+export const cdnHostname = cdn.domainName;
